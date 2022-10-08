eec63a56729bc16ca51ebf54aa160a9954d68c79
neerajkumar790652 <92433406+neerajkumar790652@users.noreply.github.com>
2022-10-08 20:23:18 +0530

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,9 +2,9 @@ const Contact = _=>{
     return(
        <section>
         <h3>📞 Contact Me</h3>
-        <a href ="https://www.linkedin.com/in/neeraj-kumar-517743192/" target="_blank">LinkedIn</a>
-        <a href ="https://www.facebook.com/neeraj.rajput.786" target="_blank">Facebook</a>
-        <a href ="https://github.com/neerajkumar790652"target="_blank">Github</a>
+        <a href ="https://www.linkedin.com/in/neeraj-kumar-517743192/" rel="noreferrer" target="_blank">LinkedIn</a>
+        <a href ="https://www.facebook.com/neeraj.rajput.786" rel="noreferrer" target="_blank">Facebook</a>
+        <a href ="https://github.com/neerajkumar790652" rel="noreferrer" target="_blank">Github</a>
         
        </section>
     )
